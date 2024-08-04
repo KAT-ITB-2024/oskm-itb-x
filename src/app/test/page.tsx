f3eef7cba0745270a77de674ef4722936fdccaf1
SandWithCheese <naufalahmad022@gmail.com>
2024-08-04 17:20:16 +0700

    feat: setup shadcn, add testing page for components

---
+++ src/app/test/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button } from "~/components/ui/button";
+
+function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
+      {/* Contoh pemakaian button, tambahkan styling sesuai kebutuhan */}
+      <div className="flex flex-col items-center justify-center gap-2">
+        <h1>Buttons</h1>
+        <div className="flex gap-4">
+          <Button variant={"pink"}>Open</Button>
+          <Button variant={"pinkoutline"}>Download Guidebook</Button>
+          <Button variant={"yellow"}>OceanLog</Button>
+          <Button variant={"merchgreen"}>Ganesha Goods &gt;</Button>
+          <Button variant={"merchyellow"}>Order Now &gt;</Button>
+          <Button variant={"merchpink"}>@ganeshagoods &gt;</Button>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Page;
