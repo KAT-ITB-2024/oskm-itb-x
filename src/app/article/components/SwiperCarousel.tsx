547b18e6a919c69d234fd332d37ddfbbcb7ee506
William Anthony <16523109@std.stei.itb.ac.id>
2024-08-14 20:28:31 +0700

    refactor: Remove unused CSS import in SwiperCarousel component

--- src/app/article/components/SwiperCarousel.tsx
+++ src/app/article/components/SwiperCarousel.tsx
@@ -6,7 +6,6 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import "src/styles/globals.css"; // Import the custom CSS
 
 const ClientOnlyCarousel: React.FC = () => {
   const [isClient, setIsClient] = useState(false);
