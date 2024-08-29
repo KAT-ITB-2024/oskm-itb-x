76e3f044b4193ea423c50b6753b08178d293a7be
Kevin Sebastian <116342133+kevinssheva@users.noreply.github.com>
2024-08-29 19:15:31 +0700

    fix: article detail

--- src/app/article/detail/[slug]/page.tsx
+++ src/app/article/detail/[slug]/page.tsx
@@ -1,97 +1,90 @@
-"use client"
+"use client";
 
-import { useParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
-import MiniArticleCarousel from 'src/app/article/components/MiniArticleCarousel';
-import { getArticle } from '~/lib/contentful/api';
-import { Article } from '~/types/articles/articleType';
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import MiniArticleCarousel from "src/app/article/components/MiniArticleCarousel";
+import { getArticle } from "~/lib/contentful/api";
+import { type Article } from "~/types/articles/articleType";
+import { FaCalendar } from "react-icons/fa6";
+import { IoMdPerson } from "react-icons/io";
+import { IoTimeOutline } from "react-icons/io5";
 
 const Page: React.FC = () => {
   const { slug } = useParams();
   const [article, setArticle] = useState<Article>();
 
   useEffect(() => {
-    getArticle({slug: slug as string}).then((data) => {
-      setArticle(data);
-    })
-  }, []);
-
-  useEffect(() => {
-    console.log(article);
-  }, [article]);
+    getArticle({ slug: slug as string })
+      .then((data) => {
+        setArticle(data as Article);
+      })
+      .catch(() => {
+        setArticle(undefined);
+      });
+  }, [slug]);
 
   return (
     <>
-      <div style={styles.pageContainer}>
-        {/* Overlay dengan warna dan opacity 40% */}
-        <div style={styles.overlay}></div>
+      <div className="w-full bg-[url('/article-icons/BgDetail.png')] py-40">
+        {article && (
+          <div className="flex flex-col items-center px-[5%]">
+            <h1 className="mb-6 text-center font-mogula text-2xl text-white md:text-5xl">
+              {article.title}
+            </h1>
 
-        {/* Spacer above the entire page content */}
-        <div style={styles.largeSpacer}></div>
-
-        {/* Tombol Go Back yang bisa diposisikan bebas */}
-        <div style={styles.goBackContainer}>
-          <a href="/article/list">
-            <button className="GoBackButton" style={styles.goBackButton}/>
-          </a>
-        </div>
-
-        {
-          article && (
-            <>
-              <div style={styles.articleTitle}>
-                <h1>{article.title}</h1>
+            <div
+              className="flex w-full max-w-lg items-center justify-between gap-6 rounded-full px-6 py-2 text-white"
+              style={styles.statsContainer}
+            >
+              <div className="flex flex-col items-center gap-2 md:flex-row">
+                <IoMdPerson />
+                <p className="text-center text-xs text-white md:text-base">
+                  {article.author}
+                </p>
               </div>
-
-              <div style={styles.articleDate}>
-                Dibuat{' '}
-                <div style={styles.statsItem}>
-                  {`${new Date(article.createdAt).toLocaleDateString("id-ID", {
+              <div className="flex flex-col items-center gap-2 md:flex-row">
+                <FaCalendar />
+                <p className="text-center text-xs text-white md:text-base">
+                  {new Date(article.createdAt).toLocaleDateString("id-ID", {
                     year: "numeric",
                     month: "long",
                     day: "numeric",
-                  })} ${new Date(article.createdAt).toLocaleTimeString("id-ID", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    second: "2-digit",
-                  })}`} WIB
-                </div>
+                  })}
+                </p>
               </div>
+              <div className="flex flex-col items-center gap-2 text-xs md:flex-row">
+                <IoTimeOutline size={20} />
+                <p className="text-center text-xs text-white md:text-base">
+                  {article.readTime} minutes
+                </p>
+              </div>
+            </div>
 
-              <div style={styles.statsContainer}>
-                <div style={styles.statsItem}>
-                  <img src="/article-icons/author.png" alt="Author" style={styles.statsIcon} /> {article.author}
-                </div>
-                <div style={styles.statsItem}>
-                  <img src="/article-icons/views_eye.png" alt="Views" style={styles.statsIcon} /> {article.views}
-                </div>
-                <div style={styles.statsItem}>
-                  <img src="/article-icons/timer.png" alt="Read Time" style={styles.statsIcon} /> {(article.readTime / 60).toFixed(0)} min read
-                </div>
-              </div>
+            <div className="relative my-10 aspect-video w-1/2 max-w-2xl overflow-hidden rounded-xl">
+              <Image
+                src={article.image.url}
+                alt={article.title}
+                fill
+                className="h-full w-full"
+                objectFit="cover"
+              />
+            </div>
 
-              <div style={styles.articleImageContainer}>
-                <img src={article.image.url} alt={article.title} style={styles.articleImage} />
-              </div>
-
-              <div style={styles.articleDescriptionContainer}>
-                <div style={styles.articleDescription}>
-                  <p>{article.description}</p>
-                </div>
-              </div>
-            </>
-          )
-        }
+            <div className="my-10 w-full max-w-[70rem] px-10 text-[#ff3e3e]">
+              <p className="text-white">{article.description}</p>
+            </div>
+          </div>
+        )}
 
         {/* Spacer before the button */}
-        <div style={styles.spacer}/>
+        <div style={styles.spacer} />
 
         {/* MiniArticleCarousel Component */}
         <MiniArticleCarousel />
 
         {/* Spacer below the button */}
-        <div style={styles.spacer}/>
-
+        <div style={styles.spacer} />
       </div>
     </>
   );
@@ -100,134 +93,89 @@ const Page: React.FC = () => {
 // Styles Page disimpan disini biar enak diatur sebelum diletakkan di Global.css//
 const styles = {
   pageContainer: {
-    maxWidth: '100%',
-    margin: '0 auto',
-    padding: '20px',
+    maxWidth: "100%",
+    margin: "0 auto",
+    padding: "20px",
     backgroundImage: `url('/article-icons/BiruBackground1.png')`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    fontFamily: 'REM, sans-serif',
-    display: 'flex',
-    flexDirection: 'column' as const,
-    alignItems: 'center', 
-    position: 'relative' as const, // penting untuk overlay
-  },
-  overlay: {
-    position: 'absolute' as const,
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: '#000D76',
-    opacity: 0.4, // Opacity 40%
-    zIndex: 0, // pastikan overlay berada di bawah konten
-  },
-  goBackContainer: {
-    position: 'absolute' as const,
-    top: '20px',  // Contoh: posisikan 20px dari atas
-    left: '20px', // Contoh: posisikan 20px dari kiri
-    zIndex: 3, // Pastikan berada di atas overlay
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    fontFamily: "REM, sans-serif",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   goBackButton: {
-    padding: '10px 20px',
-    fontSize: '1rem',
-    color: '#fff',
-    backgroundColor: '#007BFF',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: 'pointer',
+    padding: "10px 20px",
+    fontSize: "1rem",
+    color: "#fff",
+    backgroundColor: "#007BFF",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
   },
   articleTitle: {
-    fontFamily: 'Mogula, sans-serif',
-    fontSize: '64px',
-    fontWeight: '400',
-    lineHeight: '76.8px',
-    textAlign: 'center' as const,
-    marginBottom: '10px',
-    color: '#ffffff', 
+    fontFamily: "Mogula, sans-serif",
+    fontSize: "64px",
+    fontWeight: "400",
+    lineHeight: "76.8px",
+    textAlign: "center" as const,
+    marginBottom: "10px",
+    color: "#ffffff",
     zIndex: 2, // Konten di atas overlay
   },
   articleDate: {
-    fontSize: '1rem',
-    textAlign: 'center' as const,
-    marginBottom: '20px',
-    color: '#ffffff', 
+    fontSize: "1rem",
+    textAlign: "center" as const,
+    marginBottom: "20px",
+    color: "#ffffff",
     zIndex: 2,
   },
   statsContainer: {
-    display: 'flex', 
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: '0px 16px', 
-    gap: '8px',
-    borderRadius: '10cm',
-    height: '32px',
-    background: 'linear-gradient(166.31deg, rgba(255, 254, 254, 0.36) 9.26%, rgba(255, 202, 224, 0.48) 89.28%)',
-    boxShadow: '0px 0px 10px 0px #FFFFFF',
-    margin: '20px 0', 
-    color: '#ffffff',
-    zIndex: 2,
+    background:
+      "linear-gradient(166.31deg, rgba(255, 254, 254, 0.36) 9.26%, rgba(255, 202, 224, 0.48) 89.28%)",
+    boxShadow: "0px 0px 10px 0px #FFFFFF",
   },
   statsItem: {
-    display: 'inline-flex', 
-    alignItems: 'center',
-    fontSize: '1rem',
-  },
-  statsIcon: {
-    width: '20px', 
-    height: '20px', 
-    marginRight: '8px', 
+    display: "inline-flex",
+    alignItems: "center",
+    fontSize: "1rem",
   },
   articleImageContainer: {
-    textAlign: 'center' as const,
-    margin: '20px 0',
+    textAlign: "center" as const,
+    margin: "20px 0",
     zIndex: 2,
   },
   articleImage: {
-    width: '100%',
-    maxWidth: '600px',
-    height: 'auto',
-    borderRadius: '20px',
-    margin: '0 auto',
-  },
-  articleDescriptionContainer: {
-    width: '75%', 
-    padding: '10px',
-    borderRadius: '8px',
-    marginBottom: '20px',
-    margin: '0 auto',
-    color: '#ffffff', 
-    zIndex: 2,
-  },
-  articleDescription: {
-    fontSize: '1rem',
-    lineHeight: '1.5rem',
-    textAlign: 'justify' as const,
+    width: "100%",
+    maxWidth: "600px",
+    height: "auto",
+    borderRadius: "20px",
+    margin: "0 auto",
   },
   spacer: {
-    height: '50px',
+    height: "50px",
   },
   largeSpacer: {
-    height: '100px',
+    height: "100px",
   },
 
-  '@media (max-width: 768px)': {
+  "@media (max-width: 768px)": {
     statsContainer: {
-      flexDirection: 'column', 
-      borderRadius: '10cm', 
-      width: '100%',
+      flexDirection: "column",
+      borderRadius: "10cm",
+      width: "100%",
     },
     statsItem: {
-      fontSize: '0.875rem',
+      fontSize: "0.875rem",
     },
     articleTitle: {
-      fontSize: '1.5rem',
+      fontSize: "1.5rem",
     },
     articleDescriptionContainer: {
-      width: '90%', 
+      width: "90%",
     },
     articleDescription: {
-      fontSize: '0.9rem',
+      fontSize: "0.9rem",
     },
   },
 };
