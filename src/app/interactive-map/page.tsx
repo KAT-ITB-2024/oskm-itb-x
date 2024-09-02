617383beb2e96cfdeff43e1b23caddd1dd488376
Abraham Megantoro Samudra <18221123@mahasiswa.itb.ac.id>
2024-09-02 13:51:06 +0700

    Refactor: change interactive map background image to webp

--- src/app/interactive-map/page.tsx
+++ src/app/interactive-map/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default async function Page() {
   return (
-    <div className="relative min-h-screen bg-[url('/itb-x/bg-itbx.png')] px-2 pb-10 pt-28 md:px-20 md:pb-20 md:pt-36">
+    <div className="relative min-h-screen bg-[url('/itb-x/bg-itbx.webp')] bg-cover bg-no-repeat px-2 pb-10 pt-28 md:px-20 md:pb-20 md:pt-36">
       <h1 className="text-center font-mogula text-3xl text-white [text-shadow:4px_4px_10px_#0CEBCCBF] lg:text-5xl">
         Interactive Map
       </h1>
